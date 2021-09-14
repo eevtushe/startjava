8efd323677917c56f0a476d663cb2c23351813a9
eevtushe <81570750+eevtushe@users.noreply.github.com>
2021-09-14 18:03:11 +0300

    Implement_HW1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte memorySize = 8;
+        short displaySize = 15;
+        int hdd = 128;
+        long serialNumber = 12232434;
+        float cpuFrequency = 2.3f;
+        double macosVersion = 11.4d;
+        char disk = 'D';
+        boolean macos = true;
+
+        System.out.println(memorySize);
+        System.out.println(displaySize);
+        System.out.println(hdd);
+        System.out.println(serialNumber);
+        System.out.println(cpuFrequency);
+        System.out.println(macosVersion);
+        System.out.println(disk);
+        System.out.println(macos);
+    }
+}
